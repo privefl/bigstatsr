736e95bbab6e2b6907b37d2821db1db545c3c95b
Florian <florian.prive.21@gmail.com>
2016-09-15 15:20:51 +0200

    add coverage for reglin

--- tests/testthat/test.colstats.R
+++ tests/testthat/test.colstats.R
@@ -9,7 +9,6 @@ x <- matrix(rnorm(100, 0, 5), 10, 10)
 ind <- 1:5
 
 # variables to test
-ALL.TYPES <- c("char", "short", "integer", "float", "double")
 ALL.FUN <- c("sum", "mean", "sd", "var")
 
 ################################################################################
@@ -17,8 +16,9 @@ ALL.FUN <- c("sum", "mean", "sd", "var")
 test_that("Equality with matrix operations", {
   for (t in ALL.TYPES) {
     printf("\nTesting type %s\n", t)
-    X <- big.matrix(10, 10, type = t)
-    X[] <- x
+
+    X <- as.big.matrix(x, type = t)
+
     for (f in ALL.FUN) {
       eval(parse(text = sprintf(
         "expect_equal(apply(X[,], 2, %s), col%ss(X))", f, f)))
--- tests/testthat/test.reglin.R
+++ tests/testthat/test.reglin.R
@@ -9,105 +9,97 @@ N1 <- 3000
 N2 <- 1000
 x1 <- rnorm(N1, 0.5)
 x2 <- rnorm(N2, -0.5)
+weights <- c(rep(N2, N1), rep(N1, N2))
+
 x <- c(x1, x2)
+x3 <- cbind(x, x + 1, 2*x)
 
 ################################################################################
 
-get_res_weights <- function(x.big, y) {
-  res <- matrix(0, 3, ncol(x.big))
-  for (j in 1:ncol(x.big)) {
-    mylm <- lm(y ~ x.big[, j],
-               weights = c(rep(N2, N1), rep(N1, N2)))
+get_res_weights <- function(X, y) {
+  res <- matrix(0, 3, ncol(X))
+  for (j in 1:ncol(X)) {
+    mylm <- lm(y ~ X[, j], weights = weights)
     res[1:2, j] <- mylm$coefficients
     res[3, j] <- summary(mylm)$r.squared
   }
   res
 }
 
-get_res2_class <- function(x.big, y) {
-  rbind(CoeffsClass(x.big, y), RsqClass(x.big, y))
+get_res2_class <- function(X, y) {
+  rbind(CoeffsClass(X, y), RsqClass(X, y))
 }
 
 # In a case of classification
 y <- c(rep(1, N1), rep(-1, N2))
-y2 <- x + rnorm(length(x), 0, 0.1)
-x.big <- as.big.matrix(cbind(x, x+1, 2*x))
-x.big2 <- as.big.matrix(round(cbind(x, x+1, 2*x)))
-x.big3 <- as.big.matrix(round(cbind(x, x+1, 2*x)), type = "char")
 
 test_that("equality with lm in case of classification", {
-  expect_equal(get_res2_class(x.big, y),
-               get_res_weights(x.big, y))
-  expect_equal(get_res2_class(x.big2, y),
-               get_res_weights(x.big2, y))
-  expect_equal(get_res2_class(x.big3, y),
-               get_res_weights(x.big3, y))
+  for (t in ALL.TYPES) {
+    X <- as.big.matrix(x3, type = t)
+    expect_equal(get_res2_class(X, y), get_res_weights(X, y))
+  }
 })
+
 test_that("expect error from regression", {
-  expect_error(CoeffsReg(x.big, y))
-  expect_error(RsqReg(x.big, y))
+  expect_error(CoeffsReg(X, y))
+  expect_error(RsqReg(X, y))
 })
 
 ################################################################################
 
-get_res <- function(x.big, y) {
-  res <- matrix(0, 3, ncol(x.big))
-  for (j in 1:ncol(x.big)) {
-    mylm <- lm(y ~ x.big[, j])
+get_res <- function(X, y) {
+  res <- matrix(0, 3, ncol(X))
+  for (j in 1:ncol(X)) {
+    mylm <- lm(y ~ X[, j])
     res[1:2, j] <- mylm$coefficients
     res[3, j] <- summary(mylm)$r.squared
   }
   res
 }
 
-get_res2_reg <- function(x.big, y) {
-  rbind(CoeffsReg(x.big, y), RsqReg(x.big, y))
+get_res2_reg <- function(X, y) {
+  rbind(CoeffsReg(X, y), RsqReg(X, y))
 }
 
 # In a case of regression
+y2 <- x + rnorm(length(x), 0, 0.1)
+
 test_that("equality with lm in case of regression", {
-  expect_equal(get_res2_reg(x.big, y2),
-               get_res(x.big, y2))
-  expect_equal(get_res2_reg(x.big2, y2),
-               get_res(x.big2, y2))
-  expect_equal(get_res2_reg(x.big3, y2),
-               get_res(x.big3, y2))
+  for (t in ALL.TYPES) {
+    X <- as.big.matrix(x3, type = t)
+    expect_equal(get_res2_reg(X, y2), get_res(X, y2))
+  }
 })
+
 test_that("expect error from classification", {
-  expect_error(CoeffsClass(x.big, y2))
-  expect_error(RsqClass(x.big, y2))
+  expect_error(CoeffsClass(X, y2))
+  expect_error(RsqClass(X, y2))
 })
 
 ################################################################################
 
 ind.train <- sort(sample(length(x), length(x) / 2))
 
-get_res_train <- function(x.big, y) {
-  res <- matrix(0, 3, ncol(x.big))
-  for (j in 1:ncol(x.big)) {
-    mylm <- lm(y[ind.train] ~ x.big[ind.train, j])
+get_res_train <- function(X, y) {
+  res <- matrix(0, 3, ncol(X))
+  for (j in 1:ncol(X)) {
+    mylm <- lm(y[ind.train] ~ X[ind.train, j])
     res[1:2, j] <- mylm$coefficients
     res[3, j] <- summary(mylm)$r.squared
   }
   res
 }
 
-get_res2_reg_train <- function(x.big, y) {
-  rbind(CoeffsReg(x.big, y, ind.train), RsqReg(x.big, y, ind.train))
+get_res2_reg_train <- function(X, y) {
+  rbind(CoeffsReg(X, y, ind.train), RsqReg(X, y, ind.train))
 }
 
 # With only half of the data
 test_that("equality with lm in case of regression with half of the data", {
-  expect_equal(get_res2_reg_train(x.big, y2),
-               get_res_train(x.big, y2))
-  expect_equal(get_res2_reg_train(x.big2, y2),
-               get_res_train(x.big2, y2))
-  expect_equal(get_res2_reg_train(x.big3, y2),
-               get_res_train(x.big3, y2))
-})
-test_that("expect error from classification", {
-  expect_error(CoeffsClass(x.big, y2))
-  expect_error(RsqClass(x.big, y2))
+  for (t in ALL.TYPES) {
+    X <- as.big.matrix(x3, type = t)
+    expect_equal(get_res2_reg_train(X, y2), get_res_train(X, y2))
+  }
 })
 
 ################################################################################
