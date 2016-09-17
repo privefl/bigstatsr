c9cc580e7fe3f9295f1a8d2679092584fba53875
Florian <florian.prive.21@gmail.com>
2016-09-17 16:05:32 +0200

    use loop rather than copy+paste

--- tests/testthat/test.BigXYt.R
+++ tests/testthat/test.BigXYt.R
@@ -16,13 +16,16 @@ vec.scale <- rnorm(M)
 test_that("equality with tcrossprod", {
   for (t in ALL.TYPES) {
     X <- as.big.matrix(x, type = t)
-    test <- BigXYt(X = X,
-                   block.size = 10,
-                   vec.center = vec.center,
-                   vec.scale = vec.scale)
     mat <- sweep(sweep(X[,], 2, vec.center, '-'), 2, vec.scale, '/')
-    diff <- test[,] - tcrossprod(mat)
-    expect_equal(max(abs(diff)), 0)
+    for (use in c(TRUE, FALSE)) {
+      test <- BigXYt(X = X,
+                     block.size = 10,
+                     vec.center = vec.center,
+                     vec.scale = vec.scale,
+                     use.Eigen = use)
+      diff <- test[,] - tcrossprod(mat)
+      expect_equal(max(abs(diff)), 0)
+    }
   }
 })
 
@@ -32,54 +35,20 @@ test_that("equality with tcrossprod with half of the data", {
   ind <- sample(N, N/2)
   for (t in ALL.TYPES) {
     X <- as.big.matrix(x, type = t)
-    test <- BigXYt(X = X,
-                   block.size = 10,
-                   ind.train = ind,
-                   vec.center = vec.center,
-                   vec.scale = vec.scale)
     mat1 <- sweep(sweep(X[ind, ],  2, vec.center, '-'), 2, vec.scale, '/')
     mat2 <- sweep(sweep(X[-ind, ], 2, vec.center, '-'), 2, vec.scale, '/')
-    diff1 <- test[[1]][,] - tcrossprod(mat1)
-    diff2 <- test[[2]][,] - tcrossprod(mat2, mat1)
-    expect_equal(max(abs(diff1)), 0)
-    expect_equal(max(abs(diff2)), 0)
-  }
-})
-
-################################################################################
-
-test_that("equality with tcrossprod with use.Eigen=FALSE", {
-  for (t in ALL.TYPES) {
-    X <- as.big.matrix(x, type = t)
-    test <- BigXYt(X = X,
-                   block.size = 10,
-                   vec.center = vec.center,
-                   vec.scale = vec.scale,
-                   use.Eigen = FALSE)
-    mat <- sweep(sweep(X[,], 2, vec.center, '-'), 2, vec.scale, '/')
-    diff <- test[,] - tcrossprod(mat)
-    expect_equal(max(abs(diff)), 0)
-  }
-})
-
-################################################################################
-
-test_that("equality with tcrossprod with half of the data and use.Eigen = F", {
-  ind <- sample(N, N/2)
-  for (t in ALL.TYPES) {
-    X <- as.big.matrix(x, type = t)
-    test <- BigXYt(X = X,
-                   block.size = 10,
-                   ind.train = ind,
-                   vec.center = vec.center,
-                   vec.scale = vec.scale,
-                   use.Eigen = FALSE)
-    mat1 <- sweep(sweep(X[ind, ],  2, vec.center, '-'), 2, vec.scale, '/')
-    mat2 <- sweep(sweep(X[-ind, ], 2, vec.center, '-'), 2, vec.scale, '/')
-    diff1 <- test[[1]][,] - tcrossprod(mat1)
-    diff2 <- test[[2]][,] - tcrossprod(mat2, mat1)
-    expect_equal(max(abs(diff1)), 0)
-    expect_equal(max(abs(diff2)), 0)
+    for (use in c(TRUE, FALSE)) {
+      test <- BigXYt(X = X,
+                     block.size = 10,
+                     ind.train = ind,
+                     vec.center = vec.center,
+                     vec.scale = vec.scale,
+                     use.Eigen = use)
+      diff1 <- test[[1]][,] - tcrossprod(mat1)
+      diff2 <- test[[2]][,] - tcrossprod(mat2, mat1)
+      expect_equal(max(abs(diff1)), 0)
+      expect_equal(max(abs(diff2)), 0)
+    }
   }
 })
 
