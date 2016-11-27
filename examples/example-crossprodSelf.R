c18140fd87db12200e0a3af0e0ec6865de110f05
Florian Privé <florian.prive.21@gmail.com>
2016-11-27 16:18:23 +0100

    modify examples accordingly

--- examples/example-crossprodSelf.R
+++ examples/example-crossprodSelf.R
@@ -5,12 +5,12 @@ X[] <- rnorm(length(X))
 # Comparing with tcrossprod
 big_noscale <- big_scale(center = FALSE)
 test <- big_crossprodSelf(X, fun.scaling = big_noscale)
-print(dim(test))
-print(all.equal(test[,], crossprod(X[,])))
+print(dim(test$K))
+print(all.equal(test$K, crossprod(X[,])))
 
 # Using only half of the data for "training"
 ind <- sort(sample(nrow(X), nrow(X)/2))
 test2 <- big_crossprodSelf(X, fun.scaling = big_noscale,
                            ind.train = ind)
-print(dim(test2))
-print(all.equal(test2[,], crossprod(X[ind, ])))
+print(dim(test2$K))
+print(all.equal(test2$K, crossprod(X[ind, ])))
