03e82f087cea9a8b11f0b1da28e354e6f173ecae
privefl <florian.prive.21@gmail.com>
2017-01-05 15:54:02 +0100

    add dimnames to transpose

--- R/transpose.R
+++ R/transpose.R
@@ -4,8 +4,8 @@
 #' @description This function implements a simple cache-oblivious
 #' algorithm for the transposition of a "big.matrix".
 #' @inheritParams bigstatsr-package
-#' @inheritDotParams bigmemory::big.matrix -nrow -ncol -type -init
-#' @return The transposed big.matrix. Its dimension and type
+#' @inheritDotParams bigmemory::big.matrix -nrow -ncol -type -init -dimnames
+#' @return The new transposed big.matrix. Its dimension, type and dimnames
 #' are automatically determined from the input `big.matrix`.
 #' @export
 #' @examples
@@ -17,15 +17,16 @@
 #' print(identical(
 #'   t(X[,]),
 #'     Xt[,]
-#'     ))
+#' ))
 big_transpose <- function(X, ...) {
   check_X(X)
 
-  res <- big.matrix(ncol(X), nrow(X), type = typeof(X), ...)
+  res <- big.matrix(ncol(X), nrow(X), type = typeof(X),
+                    dimnames = dimnames(X)[2:1], ...)
 
   transpose3(res@address, X@address)
 
-  return(res)
+  res
 }
 
 ################################################################################
