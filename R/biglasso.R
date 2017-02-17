c2ddb03ac58338728f42da1c2ddd32b82dc627f5
privefl <florian.prive.21@gmail.com>
2017-02-17 12:48:59 +0100

    merge biglasso

--- R/biglasso.R
+++ R/biglasso.R
@@ -1,9 +1,9 @@
+################################################################################
+
 ########################################################
 ### This is a modified version from package biglasso ###
 ###      https://github.com/YaohuiZeng/biglasso      ###
 ########################################################
-
-################################################################################
 
 #' Sparse regression path
 #'
@@ -237,6 +237,62 @@ COPY_biglasso <- function(X, y.train, ind.train = 1:nrow(X), covar.train = NULL,
     col.idx = col.idx,
     rejections = rejections
   )
+}
+
+################################################################################
+
+#' Sparse linear regression
+#'
+#' Fit lasso penalized linear regression path for a `big.matrix`.
+#' Covariates can be added to correct for confounders.
+#'
+#' __This is a modified version of one function of
+#' [package biglasso](https://github.com/YaohuiZeng/biglasso)__.
+#' It adds the possibility to train to models with covariables and use many
+#' types of `big.matrix` (not only `double` ones).
+#' Yet, it only corresponds to `screen = "SSR"`.
+#'
+#' @inheritParams bigstatsr-package
+#' @inheritDotParams COPY_biglasso -X -y.train -ind.train -covar.train -family
+#'
+#' @inherit COPY_biglasso return
+#'
+#' @example examples/example-spRegLin.R
+#'
+#' @seealso [glmnet][glmnet::glmnet] [biglasso][biglasso::biglasso]
+#' @references
+#' Tibshirani, R., Bien, J., Friedman, J., Hastie, T.,
+#' Simon, N., Taylor, J. and Tibshirani, R. J. (2012),
+#' Strong rules for discarding predictors in lasso-type problems.
+#' Journal of the Royal Statistical Society:
+#' Series B (Statistical Methodology), 74: 245–266.
+#' \url{http://dx.doi.org/10.1111/j.1467-9868.2011.01004.x}.
+#'
+#' Zeng, Y., and Breheny, P. (2016). The biglasso Package: A Memory- and
+#' Computation-Efficient Solver for Lasso Model Fitting with Big Data in R.
+#' arXiv preprint arXiv:1701.05936. \url{https://arxiv.org/abs/1701.05936}.
+#'
+#' @export
+big_spRegLin <- function(X, y.train, ind.train = seq(nrow(X)),
+                         covar.train = NULL, ...) {
+  COPY_biglasso(X, y.train, ind.train, covar.train, family = "gaussian", ...)
+}
+
+################################################################################
+
+#' Sparse logistic regression
+#'
+#' @inheritParams bigstatsr-package
+#' @inheritDotParams COPY_biglasso -X -y.train -ind.train -covar.train -family
+#'
+#' @inherit big_spRegLin return description details seealso references
+#'
+#' @example examples/example-spRegLog.R
+#'
+#' @export
+big_spRegLog <- function(X, y01.train, ind.train = seq(nrow(X)),
+                         covar.train = NULL, ...) {
+  COPY_biglasso(X, y01.train, ind.train, covar.train, family = "binomial", ...)
 }
 
 ################################################################################
