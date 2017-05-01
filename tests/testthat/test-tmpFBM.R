0d89eaa04c0e7c0d4223391d140a22947e07da53
Florian Privé <florian.prive.21@gmail.com>
2017-05-01 19:12:42 +0200

    test tmpFBM

---
+++ tests/testthat/test-tmpFBM.R
@@ -0,0 +1,28 @@
+################################################################################
+
+context("TMP_FBM")
+
+opt.save <- options(bigmemory.typecast.warning = FALSE,
+                    bigmemory.default.shared = FALSE)
+
+l <- length(list.files(tempdir()))
+
+################################################################################
+
+test_that("Create temporary big.matrix objects", {
+  for (t in ALL.TYPES) {
+    tmp <- tmpFBM(init = sample(list(NULL, 1))[[1]],
+                  descriptor = sample(c(TRUE, FALSE), 1))(10, 10, type = t)
+
+    expect_equal(length(list.files(tempdir())), l + 2)
+
+    expect_equal(tmpFBM.rm(tmp), c(TRUE, TRUE))
+    expect_equal(length(list.files(tempdir())), l)
+  }
+})
+
+################################################################################
+
+options(opt.save)
+
+################################################################################
