aa1bb302836854c3c796a302b052bc78ae704000
Florian Privé <florian.prive.21@gmail.com>
2017-08-05 10:47:42 +0200

    include univReg

--- src/univRegLin5.cpp
+++ src/univRegLin5.cpp
@@ -1,59 +1,10 @@
 /******************************************************************************/
 
 #include <RcppArmadillo.h>
-#include "../inst/include/bigstatsr/SubMatAcc.h"
+#include <bigstatsr/SubMatAcc.h>
+#include <bigstatsr/univLinReg.hpp>
 
-/******************************************************************************/
-
-inline arma::vec UUty(const arma::mat& U, const arma::vec& y) {
-  // printf("address: %p\n", &Ut);
-  return U * (U.t() * y);
-}
-
-/******************************************************************************/
-
-template <class C>
-ListOf<NumericVector> univLinReg5(C macc,
-                                  const arma::mat& U,
-                                  const arma::vec& y) {
-
-  int n = macc.nrow();
-  int m = macc.ncol();
-  int K = U.n_cols;
-  myassert((int)U.n_rows == n, ERROR_DIM);
-  myassert((int)y.n_elem == n, ERROR_DIM);
-
-  arma::vec x(n), x2(n);
-  arma::vec y2 = y - UUty(U, y);
-  double y2_sumSq = dot(y2, y2);
-  double beta, x_tmp, x_diff, x2_sumSq, beta_num, beta_deno, RSS;
-  int i, j;
-
-  NumericVector betas(m), var(m);
-
-  for (j = 0; j < m; j++) {
-    for (i = 0; i < n; i++) {
-      x[i] = macc(i, j);
-    }
-    x2 = UUty(U, x);
-
-    beta_num = beta_deno = x2_sumSq = 0;
-    for (i = 0; i < n; i++) {
-      x_tmp = x[i];
-      x_diff = x_tmp - x2[i];
-      beta_num += x_tmp * y2[i];
-      beta_deno += x_tmp * x_diff;
-      x2_sumSq += x_diff * x_diff;
-    }
-    beta = beta_num / beta_deno;
-    RSS = y2_sumSq - beta * beta * x2_sumSq;
-    betas[j] = beta;
-    var[j] = RSS / (beta_deno * (n - K - 1));
-  }
-
-  return List::create(_["estim"] = betas,
-                      _["std.err"] = sqrt(var));
-}
+using namespace Rcpp;
 
 /******************************************************************************/
 
@@ -70,20 +21,26 @@ List univLinReg5(const S4& BM,
   IntegerVector cols = colInd - 1;
 
   if (Rf_inherits(BM, "BM.code")) {
-    return univLinReg5(RawSubMatAcc(*xpMat, rows, cols, BM.slot("code")),
-                       covar_U, y);
+    return bigstatsr::univLinReg5(RawSubMatAcc(*xpMat, rows, cols,
+                                               BM.slot("code")),
+                                  covar_U, y);
   } else {
     switch(xpMat->matrix_type()) {
     case 1:
-      return univLinReg5(SubMatAcc<char>(*xpMat,   rows, cols), covar_U, y);
+      return bigstatsr::univLinReg5(SubMatAcc<char>(*xpMat,   rows, cols),
+                                    covar_U, y);
     case 2:
-      return univLinReg5(SubMatAcc<short>(*xpMat,  rows, cols), covar_U, y);
+      return bigstatsr::univLinReg5(SubMatAcc<short>(*xpMat,  rows, cols),
+                                    covar_U, y);
     case 4:
-      return univLinReg5(SubMatAcc<int>(*xpMat,    rows, cols), covar_U, y);
+      return bigstatsr::univLinReg5(SubMatAcc<int>(*xpMat,    rows, cols),
+                                    covar_U, y);
     case 6:
-      return univLinReg5(SubMatAcc<float>(*xpMat,  rows, cols), covar_U, y);
+      return bigstatsr::univLinReg5(SubMatAcc<float>(*xpMat,  rows, cols),
+                                    covar_U, y);
     case 8:
-      return univLinReg5(SubMatAcc<double>(*xpMat, rows, cols), covar_U, y);
+      return bigstatsr::univLinReg5(SubMatAcc<double>(*xpMat, rows, cols),
+                                    covar_U, y);
     default:
       throw Rcpp::exception(ERROR_TYPE);
     }
--- src/univRegLog.cpp
+++ src/univRegLog.cpp
@@ -1,87 +1,10 @@
 /******************************************************************************/
 
 #include <RcppArmadillo.h>
-#include "../inst/include/bigstatsr/SubMatAcc.h"
+#include <bigstatsr/SubMatAcc.h>
+#include <bigstatsr/univLogReg.hpp>
 
-/******************************************************************************/
-
-arma::mat& getXtW(const arma::mat& covar,
-                  const arma::vec& w,
-                  arma::mat& tcovar,
-                  int n, int K) {
-  int i, k;
-
-  for (i = 0; i < n; i++) {
-    for (k = 0; k < K; k++) {
-      tcovar(k, i) = covar(i, k) * w(i);
-    }
-  }
-
-  return tcovar;
-}
-
-/******************************************************************************/
-
-template <class C>
-List IRLS(C macc,
-          arma::mat &covar,
-          const arma::vec &y,
-          const arma::vec &z0,
-          const arma::vec &w0,
-          double tol,
-          int maxiter) {
-
-  int n = macc.nrow();
-  int m = macc.ncol();
-  int K = covar.n_cols;
-  myassert((int)covar.n_rows == n, ERROR_DIM);
-  myassert((int)y.n_elem == n, ERROR_DIM);
-
-  arma::mat tcovar(K, n), tmp(K, K);
-  arma::vec Xb(n), p(n), w(n), z(n), betas_old(K), betas_new(K);
-  double diff;
-  int i, j, c;
-
-  NumericVector beta(m), var(m);
-  IntegerVector niter(m);
-
-  for (j = 0; j < m; j++) {
-    for (i = 0; i < n; i++) {
-      covar(i, 0) = macc(i, j);
-    }
-
-    c = 1;
-    z = z0;
-    w = w0;
-    tcovar = getXtW(covar, w, tcovar, n, K);
-    betas_new = solve(tcovar * covar, tcovar * z);
-
-    do {
-      c++;
-      betas_old = betas_new;
-
-      Xb = covar * betas_old;
-      p = 1 / (1 + exp(-Xb));
-      w = p % (1 - p);
-      z = Xb + (y - p) / w;
-
-      tcovar = getXtW(covar, w, tcovar, n, K);
-      betas_new = solve(tcovar * covar, tcovar * z);
-
-      diff = 2 * max(abs(betas_old - betas_new)
-                       / (abs(betas_old) + abs(betas_new)));
-    } while (diff > tol && c < maxiter);
-
-    beta[j] = betas_new(0);
-    tmp = inv(tcovar * covar);
-    var[j] = tmp(0, 0);
-    niter[j] = c;
-  }
-
-  return List::create(_["estim"] = beta,
-                      _["std.err"] = sqrt(var),
-                      _["niter"] = niter);
-}
+using namespace Rcpp;
 
 /******************************************************************************/
 
@@ -102,25 +25,25 @@ List IRLS(const S4& BM,
   IntegerVector cols = colInd - 1;
 
   if (Rf_inherits(BM, "BM.code")) {
-    return IRLS(RawSubMatAcc(*xpMat, rows, cols, BM.slot("code")),
-                covar, y, z0, w0, tol, maxiter);
+    return bigstatsr::IRLS(RawSubMatAcc(*xpMat, rows, cols, BM.slot("code")),
+                           covar, y, z0, w0, tol, maxiter);
   } else {
     switch(xpMat->matrix_type()) {
     case 1:
-      return IRLS(SubMatAcc<char>(*xpMat,   rows, cols),
-                  covar, y, z0, w0, tol, maxiter);
+      return bigstatsr::IRLS(SubMatAcc<char>(*xpMat,   rows, cols),
+                             covar, y, z0, w0, tol, maxiter);
     case 2:
-      return IRLS(SubMatAcc<short>(*xpMat,  rows, cols),
-                  covar, y, z0, w0, tol, maxiter);
+      return bigstatsr::IRLS(SubMatAcc<short>(*xpMat,  rows, cols),
+                             covar, y, z0, w0, tol, maxiter);
     case 4:
-      return IRLS(SubMatAcc<int>(*xpMat,    rows, cols),
-                  covar, y, z0, w0, tol, maxiter);
+      return bigstatsr::IRLS(SubMatAcc<int>(*xpMat,    rows, cols),
+                             covar, y, z0, w0, tol, maxiter);
     case 6:
-      return IRLS(SubMatAcc<float>(*xpMat,  rows, cols),
-                  covar, y, z0, w0, tol, maxiter);
+      return bigstatsr::IRLS(SubMatAcc<float>(*xpMat,  rows, cols),
+                             covar, y, z0, w0, tol, maxiter);
     case 8:
-      return IRLS(SubMatAcc<double>(*xpMat, rows, cols),
-                  covar, y, z0, w0, tol, maxiter);
+      return bigstatsr::IRLS(SubMatAcc<double>(*xpMat, rows, cols),
+                             covar, y, z0, w0, tol, maxiter);
     default:
       throw Rcpp::exception(ERROR_TYPE);
     }
