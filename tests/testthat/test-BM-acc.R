15758927c61d7d62f15af758ce609b32362ede7e
Florian <florian.prive.21@gmail.com>
2017-08-26 13:28:44 +0200

    factorize test acc

--- tests/testthat/test-BM-acc.R
+++ tests/testthat/test-BM-acc.R
@@ -2,12 +2,31 @@
 
 context("BM_ACC")
 
+test_acc <- function(call) {
+  call <- deparse(substitute(call))
+  eval(parse(text = sprintf("expect_equal(%s, %s)",
+                            sub("^mat", "X", call),
+                            sub("^mat", "x", call))), parent.frame())
+}
+
 x0 <- matrix(rnorm(256, mean = 100, sd = 10), 16)
 
-for (t in TEST.TYPES) {
-  X <- big_copy(x0, type = t)
-  expect_s4_class(X, "FBM")
-  x <- X[]
+################################################################################
+
+for (t in c(TEST.TYPES, "FBM.code256")) {
+
+  if (t == "FBM.code256") {
+    X <- big_copy(x0, type = "raw")
+    X[] <- as.raw(0:255)
+    X <- add_code256(X, code = as.vector(x0))
+    expect_s4_class(X, "FBM")
+    expect_s4_class(X, "FBM.code256")
+    x <- x0
+  } else {
+    X <- big_copy(x0, type = t)
+    expect_s4_class(X, "FBM")
+    x <- X[]
+  }
 
   test_that("same dimensions", {
     expect_equal(nrow(X), nrow(x))
@@ -18,38 +37,37 @@ for (t in TEST.TYPES) {
 
   test_that("same accessing", {
 
-    expect_equal(X[], x[])
-    expect_equal(X[], x[])
-    expect_equal(X[, , drop = FALSE], x[, , drop = FALSE])
-    expect_equal(X[, , drop = TRUE], x[, , drop = TRUE])
-    expect_equal(X[1, ], x[1, ])
-    expect_equal(X[1, , drop = FALSE], x[1, , drop = FALSE])
-    expect_equal(X[1, , drop = TRUE], x[1, , drop = TRUE])
-    expect_equal(X[cbind(1:5, 1:5)], x[cbind(1:5, 1:5)])
+    test_acc(mat[])
+    test_acc(mat[, , drop = FALSE])
+    test_acc(mat[, , drop = TRUE])
+    test_acc(mat[1, ])
+    test_acc(mat[1, , drop = FALSE])
+    test_acc(mat[1, , drop = TRUE])
+    test_acc(mat[cbind(1:5, 1:5)])
 
     for (ind in list(1:5, -(1:5), c(TRUE, FALSE, TRUE))) {
 
-      expect_equal(X[ind, ], x[ind, ])
-      expect_equal(X[ind, , drop = FALSE], x[ind, , drop = FALSE])
-      expect_equal(X[ind, , drop = TRUE], x[ind, , drop = TRUE])
-      expect_equal(X[, 1], x[, 1])
-      expect_equal(X[, 1, drop = FALSE], x[, 1, drop = FALSE])
-      expect_equal(X[, 1, drop = TRUE], x[, 1, drop = TRUE])
-      expect_equal(X[1, 1], x[1, 1])
-      expect_equal(X[1, 1, drop = FALSE], x[1, 1, drop = FALSE])
-      expect_equal(X[1, 1, drop = TRUE], x[1, 1, drop = TRUE])
-      expect_equal(X[ind, 1], x[ind, 1])
-      expect_equal(X[ind, 1, drop = FALSE], x[ind, 1, drop = FALSE])
-      expect_equal(X[ind, 1, drop = TRUE], x[ind, 1, drop = TRUE])
-      expect_equal(X[, ind], x[, ind])
-      expect_equal(X[, ind, drop = FALSE], x[, ind, drop = FALSE])
-      expect_equal(X[, ind, drop = TRUE], x[, ind, drop = TRUE])
-      expect_equal(X[1, ind], x[1, ind])
-      expect_equal(X[1, ind, drop = FALSE], x[1, ind, drop = FALSE])
-      expect_equal(X[1, ind, drop = TRUE], x[1, ind, drop = TRUE])
-      expect_equal(X[ind, ind], x[ind, ind])
-      expect_equal(X[ind, ind, drop = FALSE], x[ind, ind, drop = FALSE])
-      expect_equal(X[ind, ind, drop = TRUE], x[ind, ind, drop = TRUE])
+      test_acc(mat[ind, ])
+      test_acc(mat[ind, , drop = FALSE])
+      test_acc(mat[ind, , drop = TRUE])
+      test_acc(mat[, 1])
+      test_acc(mat[, 1, drop = FALSE])
+      test_acc(mat[, 1, drop = TRUE])
+      test_acc(mat[1, 1])
+      test_acc(mat[1, 1, drop = FALSE])
+      test_acc(mat[1, 1, drop = TRUE])
+      test_acc(mat[ind, 1])
+      test_acc(mat[ind, 1, drop = FALSE])
+      test_acc(mat[ind, 1, drop = TRUE])
+      test_acc(mat[, ind])
+      test_acc(mat[, ind, drop = FALSE])
+      test_acc(mat[, ind, drop = TRUE])
+      test_acc(mat[1, ind])
+      test_acc(mat[1, ind, drop = FALSE])
+      test_acc(mat[1, ind, drop = TRUE])
+      test_acc(mat[ind, ind])
+      test_acc(mat[ind, ind, drop = FALSE])
+      test_acc(mat[ind, ind, drop = TRUE])
     }
   })
 }
