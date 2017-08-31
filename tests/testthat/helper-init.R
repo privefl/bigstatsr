3b42a980323d8613e6509c238ff4e26e1eb8821c
Florian <florian.prive.21@gmail.com>
2017-08-31 14:30:15 +0200

    no longer parallel on CRAN

--- tests/testthat/helper-init.R
+++ tests/testthat/helper-init.R
@@ -12,9 +12,9 @@ library(Matrix)
 test_cores <- function() {
 
   is.cran      <- !identical(Sys.getenv("BIGSTATSR_CRAN"), "false")
-  is.randomSVD <- (get_reporter()$.context == "RANDOM_SVD")
+  # is.randomSVD <- (get_reporter()$.context == "RANDOM_SVD")
 
-  `if`(is.cran && is.randomSVD, 1, sample(2, size = 1))
+  `if`(is.cran, 1, sample(2, size = 1))  # && is.randomSVD
 }
 
 ################################################################################
