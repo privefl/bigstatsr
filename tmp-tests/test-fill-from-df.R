0b072bb1727b177272efb3edf55578e361e0a1f7
Florian Privé <florian.prive.21@gmail.com>
2018-07-27 22:17:21 +0200

    redo laptop

--- tmp-tests/test-fill-from-df.R
+++ tmp-tests/test-fill-from-df.R
@@ -1,47 +1,41 @@
 library(bigstatsr)
 
-# mtcars <- datasets::mtcars
-# mtcars <- mtcars[rep(1:32, 500), rep(1:11, 500)]  # 2687 Mb
-# X <- FBM(nrow(mtcars), ncol(mtcars), backingfile = "tmp-data/mtcars-big", save = TRUE)
+mtcars <- datasets::mtcars
+mtcars <- mtcars[rep(1:32, 500), rep(1:11, 500)]  # 2687 Mb
+# X <- FBM(nrow(mtcars), ncol(mtcars), init = 0, backingfile = "tmp-data/mtcars-big", save = TRUE)
 X <- big_attach("tmp-data/mtcars-big.rds")
 
-tmp <- gc(reset = TRUE)
-system.time({
-  for (j in cols_along(mtcars)) {
-    X[, j] <- mtcars[[j]]
+fill_df1 <- compiler::cmpfun(function(X, df) {
+  for (j in cols_along(X)) {
+    X[, j] <- df[[j]]
   }
-}) # 5 sec  -> too long  //  3.8
-gc() - tmp # 337 Mb  // 144 Mb
+})
+
+tmp <- gc(reset = TRUE)
+system.time(fill_df1(X, mtcars)) # 3 sec
+gc() - tmp # 123 Mb
 
 
 tmp <- gc(reset = TRUE)
-system.time({
-  X[] <- as.matrix(mtcars)
-}) # 2.8 sec  //  5.8
-gc() - tmp # 2687 Mb  -> too much memory (full copy)  // 672
+system.time(X[] <- as.matrix(mtcars)) # 2.5 sec
+gc() - tmp # 672 Mb
 
 
 Rcpp::sourceCpp('tmp-tests/test-fill-from-df.cpp')
 
 tmp <- gc(reset = TRUE)
-system.time({
-  df2FBM(X, mtcars, rows_along(X3), cols_along(X3))
-}) # 1.7 sec  // 4.5
-gc() - tmp  # 0 Mb  -> perfect
+system.time(df2FBM(X, mtcars, rows_along(X), cols_along(X))) # 1.7 sec  // 2.3
+gc() - tmp  # 0 Mb
 
 microbenchmark::microbenchmark(
-  LOOP = {
-    for (j in cols_along(mtcars)) {
-      X[, j] <- mtcars[[j]]
-    }
-  },
+  LOOP = fill_df1(X, mtcars),
   AS_MAT = X[] <- as.matrix(mtcars),
-  RCPP = df2FBM(X, mtcars, rows_along(X3), cols_along(X3)),
+  RCPP = df2FBM(X, mtcars, rows_along(X), cols_along(X)),
   times = 10
 )
-## Laptop:
+## Laptop 500:
 # Unit: seconds
 #   expr      min       lq     mean   median       uq      max neval
-#   LOOP 2.775548 2.862055 2.900700 2.889623 2.930390 3.047641    10
-# AS_MAT 1.962036 1.993375 2.038083 2.024001 2.096362 2.117451    10
-#   RCPP 1.193567 1.321788 1.616395 1.501757 1.611372 2.389710    10
+#   LOOP 2.854036 2.916772 2.978472 2.964080 3.019227 3.145147    10
+# AS_MAT 1.991628 2.017892 2.068286 2.055746 2.107459 2.207610    10
+#   RCPP 1.230471 1.335128 1.451531 1.486146 1.495760 1.633525    10
