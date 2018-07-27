0d80506e54b3f04edf400961d71673b2b0e5d8e0
Florian Privé <florian.prive.21@gmail.com>
2018-07-27 22:04:20 +0200

    bench fill df

--- tmp-tests/test-fill-from-df.R
+++ tmp-tests/test-fill-from-df.R
@@ -1,32 +1,47 @@
 library(bigstatsr)
 
-mtcars <- datasets::mtcars
-mtcars <- mtcars[rep(1:32, 1000), rep(1:11, 1000)]  # 2687 Mb
-
+# mtcars <- datasets::mtcars
+# mtcars <- mtcars[rep(1:32, 500), rep(1:11, 500)]  # 2687 Mb
+# X <- FBM(nrow(mtcars), ncol(mtcars), backingfile = "tmp-data/mtcars-big", save = TRUE)
+X <- big_attach("tmp-data/mtcars-big.rds")
 
 tmp <- gc(reset = TRUE)
-X <- FBM(nrow(mtcars), ncol(mtcars))
 system.time({
   for (j in cols_along(mtcars)) {
     X[, j] <- mtcars[[j]]
   }
-}) # 5 sec  -> too long
-gc() - tmp # 337 Mb
+}) # 5 sec  -> too long  //  3.8
+gc() - tmp # 337 Mb  // 144 Mb
 
 
 tmp <- gc(reset = TRUE)
-X2 <- FBM(nrow(mtcars), ncol(mtcars))
 system.time({
-  X2[] <- as.matrix(mtcars)
-}) # 2.8 sec
-gc() - tmp # 2687 Mb  -> too much memory (full copy)
+  X[] <- as.matrix(mtcars)
+}) # 2.8 sec  //  5.8
+gc() - tmp # 2687 Mb  -> too much memory (full copy)  // 672
 
 
 Rcpp::sourceCpp('tmp-tests/test-fill-from-df.cpp')
 
 tmp <- gc(reset = TRUE)
-X3 <- FBM(nrow(mtcars), ncol(mtcars))
 system.time({
-  df2FBM(X3, mtcars, rows_along(X3), cols_along(X3))
-}) # 1.7 sec
+  df2FBM(X, mtcars, rows_along(X3), cols_along(X3))
+}) # 1.7 sec  // 4.5
 gc() - tmp  # 0 Mb  -> perfect
+
+microbenchmark::microbenchmark(
+  LOOP = {
+    for (j in cols_along(mtcars)) {
+      X[, j] <- mtcars[[j]]
+    }
+  },
+  AS_MAT = X[] <- as.matrix(mtcars),
+  RCPP = df2FBM(X, mtcars, rows_along(X3), cols_along(X3)),
+  times = 10
+)
+## Laptop:
+# Unit: seconds
+#   expr      min       lq     mean   median       uq      max neval
+#   LOOP 2.775548 2.862055 2.900700 2.889623 2.930390 3.047641    10
+# AS_MAT 1.962036 1.993375 2.038083 2.024001 2.096362 2.117451    10
+#   RCPP 1.193567 1.321788 1.616395 1.501757 1.611372 2.389710    10
--- tmp-tests/test-fill-from-df.cpp
+++ tmp-tests/test-fill-from-df.cpp
@@ -1,6 +1,8 @@
 // [[Rcpp::depends(BH, bigstatsr)]]
 #include <bigstatsr/BMAcc.h>
 
+// [[Rcpp::plugins(unwindProtect)]]
+
 // [[Rcpp::export]]
 void df2FBM(Environment X, DataFrame x,
             const IntegerVector& ind_row,
@@ -8,13 +10,14 @@ void df2FBM(Environment X, DataFrame x,
 
   XPtr<FBM> xptr = X["address"];
   SubBMAcc<double> macc(xptr, ind_row - 1, ind_col - 1);
-  NumericVector col;
 
-  for (size_t j = 0; j < macc.ncol(); j++) {
+  size_t i, j, n = macc.nrow(), m = macc.ncol();
+  NumericVector col(n);
+
+  for (j = 0; j < m; j++) {
     col = as<NumericVector>(x[j]);
-    for (size_t i = 0; i < macc.nrow(); i++) {
+    for (i = 0; i < n; i++) {
       macc(i, j) = col[i];
     }
   }
 }
-
