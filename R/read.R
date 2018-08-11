8fadc54df7e68d6a7b0160933ed8450ea3d10b23
Florian Privé <florian.prive.21@gmail.com>
2018-08-11 23:43:00 +0200

    simplify big_read

--- R/read.R
+++ R/read.R
@@ -15,9 +15,6 @@ drop_ext <- function(file) tools::file_path_sans_ext(file)
 #' @param backingfile Path to the file storing the Big Matrix on disk.
 #'   An extension ".bk" will be automatically added.
 #'   Default uses `file` without its extension.
-#' @param nb_parts Number of parts in which to split reading.
-#'   Parts are referring to blocks of selected columns.
-#'   Default use global option `bigstatsr.block.sizeGB` to set a good value.
 #' @inheritParams bigreadr::big_fread2
 #' @inheritParams FBM
 #' @param progress Show progress? Default is `TRUE`.
@@ -38,51 +35,31 @@ big_read <- function(file, select,
                      progress = TRUE,
                      ...) {
 
-  assert_exist(file)
-  assert_int(select); assert_pos(select)
-  if (is.unsorted(select, strictly = TRUE))
-    stop2("Argument 'select' should be sorted.")
+  # Prepare reading
+  X <- NULL
+  offset <- 0
+  fill_FBM <- function(df) {
 
-  # Need package {bigreadr}
-  if (!requireNamespace("bigreadr", quietly = TRUE) ||
-      utils::packageVersion("bigreadr") < package_version("0.1.1"))
-    stop2("Please install package {bigreadr} (>= 0.1.1).")
-
-  # Number of parts
-  if (is.null(nb_parts)) {
-    nb_parts <- ceiling(
-      file.size(file) / (getOption("bigstatsr.block.sizeGB") * 2^29))
-    if (progress) message2("Will read the file in %d parts.", nb_parts)
-  }
-
-  if (progress) {
-    pb <- utils::txtProgressBar(min = 0, max = length(select), style = 3)
-    on.exit(close(pb), add = TRUE)
+    # Filter rows
+    if (!is.null(filter)) df <- df[filter, , drop = FALSE]
+    # Initialize FBM on first round
+    if (offset == 0) {
+      # Resulting FBM
+      X <<- FBM(nrow(df), length(select), type = type, init = NULL,
+                backingfile = backingfile, create_bk = TRUE)$save()
+    }
+    # Fill part of the FBM
+    ind <- cols_along(df)
+    X[, offset + ind] <- df
+    offset <<- offset + length(ind)
+    # Return colnames
+    names(df)
   }
 
   # Read and fill by parts
-  X <- NULL
-  offset <- 0
   colnames <- bigreadr::big_fread2(
-    file, nb_parts, skip = skip, select = select, .transform = function(df) {
-
-      # Filter rows
-      if (!is.null(filter)) df <- df[filter, , drop = FALSE]
-      # Initialize FBM on first round
-      if (offset == 0) {
-        # Resulting FBM
-        if (is.null(nrow(df))) print(filter)
-        X <<- FBM(nrow(df), length(select), type = type, init = NULL,
-                  backingfile = backingfile, create_bk = TRUE)$save()
-      }
-      # Fill part of the FBM
-      ind <- cols_along(df)
-      X[, offset + ind] <- df
-      offset <<- offset + length(ind)
-      if (progress) utils::setTxtProgressBar(pb, offset)
-      # Return colnames
-      names(df)
-    }, .combine = unlist, showProgress = FALSE)
+    file, nb_parts, skip = skip, select = select, progress = progress,
+    .transform = fill_FBM, .combine = unlist, ... = ...)
 
   # Returns
   structure(X, fbm_names = colnames)
