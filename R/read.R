2b3de0f09f3e03f74b2efa0e1a13ce8061fc6d04
Florian Privé <florian.prive.21@gmail.com>
2018-08-12 10:34:53 +0200

    pass args as ...

--- R/read.R
+++ R/read.R
@@ -9,30 +9,24 @@ drop_ext <- function(file) tools::file_path_sans_ext(file)
 #' Read a file as a Filebacked Big Matrix by using package {bigreadr}.
 #' For a mini-tutorial, please see [this vignette](https://goo.gl/91oNxU).
 #'
+#' @param file File to read.
 #' @param select Indices of columns to read (sorted).
 #'   The length of `select` will be the number of columns of the resulting FBM.
 #' @param filter Vector used to subset the rows of each data frame.
 #' @param backingfile Path to the file storing the Big Matrix on disk.
 #'   An extension ".bk" will be automatically added.
 #'   Default uses `file` without its extension.
-#' @inheritParams bigreadr::big_fread2
+#' @inheritDotParams bigreadr::big_fread2 -file -select -.transform -.combine
 #' @inheritParams FBM
-#' @param progress Show progress? Default is `TRUE`.
-#' @param ... More arguments to be passed to [data.table::fread].
 #'
-#' @return A Filebacked Big Matrix of type '<type>' with <nrow> rows
-#'   and <length(select)> columns.
+#' @return A Filebacked Big Matrix of type '<type>' with <length(select)> columns.
 #'
 #' @export
 #'
-big_read <- function(file, select,
-                     nb_parts = NULL,
-                     filter = NULL,
+big_read <- function(file, select, filter = NULL,
                      type = c("double", "integer", "unsigned short",
                               "unsigned char", "raw"),
                      backingfile = drop_ext(file),
-                     skip = 0,
-                     progress = TRUE,
                      ...) {
 
   # Prepare reading
@@ -58,8 +52,7 @@ big_read <- function(file, select,
 
   # Read and fill by parts
   colnames <- bigreadr::big_fread2(
-    file, nb_parts, skip = skip, select = select, progress = progress,
-    .transform = fill_FBM, .combine = unlist, ... = ...)
+    file, select = select, .transform = fill_FBM, .combine = unlist, ... = ...)
 
   # Returns
   structure(X, fbm_names = colnames)
