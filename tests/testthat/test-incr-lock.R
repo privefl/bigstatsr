564fcc70c321bd1f2883de540913582c33c47b50
Florian Privé <florian.prive.21@gmail.com>
2020-01-09 14:03:19 +0100

    skip on Solaris

--- tests/testthat/test-incr-lock.R
+++ tests/testthat/test-incr-lock.R
@@ -4,20 +4,21 @@ context("INCR_LOCK")
 
 ################################################################################
 
-mat2 <- FBM(1, 1, init = 0)
-expect_identical(mat2[], 0)
+testthat::test_that("Locks work for parallel incrementation", {
 
-N <- round(runif(1, 100, 1000))
+  skip_on_os("solaris")
 
-################################################################################
+  mat2 <- FBM(1, 1, init = 0)
+  N <- round(runif(1, 100, 1000))
 
-registerDoParallel(cl <- makeCluster(2))
-test <- foreach(k = 1:N, .combine = 'c') %dopar% {
-  bigstatsr::big_increment(mat2, k, use_lock = TRUE)
-}
-stopCluster(cl)
+  registerDoParallel(cl <- makeCluster(2))
+  test <- foreach(k = 1:N, .combine = 'c') %dopar% {
+    bigstatsr::big_increment(mat2, k, use_lock = TRUE)
+  }
+  stopCluster(cl)
 
-expect_identical(test, NULL)
-expect_identical(mat2[], sum(1:N) + 0)
+  expect_identical(test, NULL)
+  expect_identical(mat2[], sum(1:N) + 0)
+})
 
 ###############################################################################
