8a3a44fc6a192b1d18ce014846f3f7bdd368d423
Florian Privé <florian.prive.21@gmail.com>
2024-08-26 15:29:55 +0200

    fix NOTE on {rmio} not being imported from

--- R/FBM.R
+++ R/FBM.R
@@ -110,6 +110,7 @@ sub_bk <- function(path, replacement = "", stop_if_not_ext = TRUE) {
 #' X[]  # access as standard R matrix
 #'
 #' @exportClass FBM
+#' @importFrom rmio file_create file_resize_off
 #'
 FBM_RC <- methods::setRefClass(
 
